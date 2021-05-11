e68689df4c2040cbb2f32a331abe0b07cbfb62bc
jaredgod <72107036+jaredgod@users.noreply.github.com>
2021-05-10 20:51:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import translater
 app = app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return translater.weirdify('Hello World')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
