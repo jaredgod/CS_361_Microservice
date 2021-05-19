8b47b7410d1b71279be2e8f96cb56a7b637f33f3
jaredgod <72107036+jaredgod@users.noreply.github.com>
2021-05-18 17:45:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask
 import translater
 
 app = Flask(__name__)
+System.err.println("Hello, logs!");
 
 @app.route('/')
 def index():
-    System.err.println("Hello, logs!");
+    System.err.println("request recieved");
     json_data = flask.request.json
     data = json_data["data"]
+    System.err.println("response sent");
     return translater.weirdify(data)
 
 if __name__ == '__main__':
