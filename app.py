db08852306b5b77a07544d4e95b3f7011f8b103f
jaredgod <72107036+jaredgod@users.noreply.github.com>
2021-05-18 17:42:12 -0700

    test logs

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import flask
 from flask import Flask
 import translater
 
-app = app = Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    System.err.println("Hello, logs!");
+    json_data = flask.request.json
+    data = json_data["data"]
+    return translater.weirdify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=33507)
